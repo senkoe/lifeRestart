a09ea6acab9c57a8efa182533edb86a0afab6dfa
senkoe <90040582+senkoe@users.noreply.github.com>
2021-09-03 18:54:11 +0800

    Update talent.js

--- src/talent.js
+++ src/talent.js
@@ -59,7 +59,7 @@ class Talent {
             .fill(1).map((v, i)=>{
                 if(!i && include) return include;
                 const gradeRandom = Math.random();
-                let grade;
+                let grade = 3;
                 if(gradeRandom>=0.111) grade = 0;
                 else if(gradeRandom>=0.011) grade = 1;
                 else if(gradeRandom>=0.001) grade = 2;
